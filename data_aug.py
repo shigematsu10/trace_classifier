8ac0e40630a7044a93c19887793b6e623f470952
shigematsu10 <shigematsu10kuma@gmail.com>
2022-04-26 17:19:13 +0900

    Data Augmentationの基本操作追加

---
+++ data_aug.py
@@ -0,0 +1,50 @@
+from turtle import color
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
+
+
+def show_image(img):
+    plt.figure(figsize=(5, 5))
+    plt.xticks(color='None')
+    plt.yticks(color='None')
+    plt.tick_params(bottom=False, left =False)
+    plt.imshow(img)
+    plt.show()
+    plt.close()
+
+#<<<--------------------------------基本操作----------------------------------------------->>>
+#読み込む時点でresizeも行う
+img1 = image.load_img('./images/red_draw/draw0.jpeg', target_size=(150, 100))
+img2 = image.load_img('./images/red_draw/draw1.jpeg', target_size=(150, 100))
+#画像からndarrayに変換
+img1 = np.array(img1)
+img2 = np.array(img2)
+#(縦，横，チャンネル数)の3次元データ
+#print(img1.shape)
+
+
+#Keras.ImageDataGeneratorは4次元データに対応＝複数枚の入力に対応(画像数，縦，横，チャンネル数)
+img1 = img1[np.newaxis, :, :, :]
+img2 = img2[np.newaxis, :, :, :]
+#(画像数，縦，横，チャンネル数)の4次元データ
+#print(img1.shape)
+dataset = np.append(img1, img2, axis=0)
+print(dataset.shape)
+
+#画像変換方法定義
+datagen = image.ImageDataGenerator(rotation_range=180)
+#画像生成
+dataset_aug = datagen.flow(dataset, batch_size=1)
+#ndarrayから画像へ変換（引数の一つ目注意：縦，横，チャンネル数の3次元）
+data1 = array_to_img(dataset_aug[0][0], scale = False)
+data2 = array_to_img(dataset_aug[1][0], scale = False)
+show_image(data1)
+show_image(data2)
+
+#画像保存
+image.save_img('./sample.jpeg', data1)
+image.save_img('./sample2.jpeg', data2)
+#<<<---------------------------------------------------------------------------------------->>>
+
--- sample.py
+++
@@ -1,2 +0,0 @@
-print('確認です') 
-print('vscode　からの操作確認です')
