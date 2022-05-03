4db619ba34d2d193c501ebd4b2135cce4014601f
shigematsu10 <shigematsu10kuma@gmail.com>
2022-05-03 15:13:15 +0900

    画像変更

--- data_aug.py
+++ data_aug.py
@@ -3,5 +3,5 @@ from DataAugmentation.augmentation import ImageProcess
 if __name__ == '__main__':
     path_name_list = ['rain']
     for path_name in path_name_list :
-        img_proc = ImageProcess(path_name, resize_original_save=False)
+        img_proc = ImageProcess(path_name, resize_original_save=True)
         img_proc(aug_method='all')
--- input.py
+++ input.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     i=0
-    im_gray = cv2.imread('./images/red_rain/rain1.jpg', cv2.IMREAD_GRAYSCALE)
+    im_gray = cv2.imread('./images/red_rain/aug/rain1.jpg', cv2.IMREAD_GRAYSCALE)
     #print(im_gray.shape)
     cv2.imshow('gray', im_gray)
     cv2.waitKey(0)
